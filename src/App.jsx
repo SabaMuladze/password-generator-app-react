1475b7423cdb13bec1de2bd642c65eab7e87e720
SabaMuladze <muladze66@bk.ru>
2023-08-22 22:14:38 +0400

    style:add custom  checkboxes, button and strength section

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,27 @@ function App() {
               <span className='fillInp' style={{ width: inpval * 5 + '%' }}></span>
 
             </form>
+            <div className='checkbox'>
+              <input type="checkbox" id='check' />
+              <p>Include Uppercase Letters</p>
+            </div>
+            <div className='checkbox'>
+              <input type="checkbox" id='check' />
+              <p>Include Uppercase Letters</p>
+            </div>
+            <div className='checkbox'>
+              <input type="checkbox" id='check' />
+              <p>Include Uppercase Letters</p>
+            </div>
+            <div className='checkbox'>
+              <input type="checkbox" id='check' />
+              <p>Include Uppercase Letters</p>
+            </div>
+            <div className='strength'>
+              <p className='strengthP'>STRENGTH</p>
+              <div>s</div>
+            </div>
+            <button type='submit' className='btn'>GENERATE <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg"><path fill="#24232C" d="m5.106 12 6-6-6-6-1.265 1.265 3.841 3.84H.001v1.79h7.681l-3.841 3.84z" /></svg></button>
           </div>
         </main>
       </div>
